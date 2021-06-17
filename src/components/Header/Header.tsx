52a97ecf5bad136d1fb112f2a5566b075201f31f
domask2 <domask2@mail.ru>
2021-06-17 09:35:51 +0300

    init project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from './Header.style';
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <h2>Header</h2>
+    </Wrapper>
+  );
+};
+
+export default Header;
