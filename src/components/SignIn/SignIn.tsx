c6f9bba1f8f0c06efd554d1fc5f3144792ae0ba6
domask2 <domask2@mail.ru>
2021-06-23 15:15:42 +0300

    add login

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,11 +1,23 @@
 import { Wrapper } from './SignIn.style';
 import Button from './../forms/Button/Button';
-import { signInWithGoogle } from './../../firebase/utils';
+import { signInWithGoogle, auth } from './../../firebase/utils';
 import FormInput from '../forms/FormInput/FormInput';
+import useInput from '../../hooks/useInput';
 
 const SignIn: React.FC = () => {
+  const email = useInput('');
+  const password = useInput('');
+
   async function handleSubmit(e: any) {
     e.preventDefault();
+
+    try {
+      await auth.signInWithEmailAndPassword(email.value, password.value);
+      email.reset();
+      password.reset();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -14,8 +26,20 @@ const SignIn: React.FC = () => {
         <h2>LogIn</h2>
         <div className="form-wrapper">
           <form onSubmit={handleSubmit}>
+            <FormInput type="email" name="email" placeholder="Email" displayName={email} />
+
+            <FormInput
+              type="password"
+              name="password"
+              placeholder="Password"
+              displayName={password}
+            />
+
             <div className="socialSignIn">
               <div className="row">
+                <Button type="submit" pd="16px">
+                  LOGIN
+                </Button>
                 <Button onClick={signInWithGoogle} otherProps="1">
                   <span className="icon"></span>
                   <p>Google</p>
