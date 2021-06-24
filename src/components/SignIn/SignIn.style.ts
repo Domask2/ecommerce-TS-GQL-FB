6adbf4be84cb8143cc9aa62f6570c3bd2202e880
Domask <domask2@mail.ru>
2021-06-24 19:25:29 +0300

    full recovery password

--- src/components/SignIn/SignIn.style.ts
+++ src/components/SignIn/SignIn.style.ts
@@ -17,4 +17,12 @@ export const SocialSignIn = styled.section`
   }
 `;
 
-export const Links = styled.section``;
+export const Links = styled.section`
+  display: block;
+  width: 100%;
+  margin: 1.5rem auto 0;
+
+  a {
+    color: black
+  }
+`;
