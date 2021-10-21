dc9ec1723d39393dee07fc32ddfaf9f340d71ac2
Serg <domask2@mail.ru>
2021-10-21 15:49:11 +0300

    add OrderHistory Component

---
+++ src/components/OrderHistory/OrderHistory.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+} from '@material-ui/core';
+import moment from 'moment';
+
+interface IProps {
+  orders: [];
+}
+
+const columns = [
+  {
+    id: 'orderCreateDate',
+    lable: 'Order Date',
+  },
+  {
+    id: 'documentID',
+    lable: 'Order ID',
+  },
+  {
+    id: 'orderTotal',
+    lable: 'Amount',
+  },
+];
+
+const styles = {
+  fontSize: '16px',
+  cursor: 'pointer',
+  width: '10%',
+};
+
+const formatText = (columnName: string, columnValue: string) => {
+  switch (columnName) {
+    case 'orderTotal':
+      return `${columnValue}`;
+    case 'orderCreateDate':
+      return moment(columnValue).format('DD/MM/YYYY');
+    default:
+      return columnValue;
+  }
+};
+
+const OrderHistory: React.FC<IProps> = ({ orders }) => {
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columns.map((column, pos) => {
+              const { lable } = column;
+              return (
+                <TableCell key={pos} style={styles}>
+                  {lable}
+                </TableCell>
+              );
+            })}
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {Array.isArray(orders) &&
+            orders.length > 0 &&
+            orders.map((row, pos) => {
+              return (
+                <TableRow key={pos}>
+                  {columns.map((column, pos) => {
+                    const columnName = column.id;
+                    const columnValue = row[columnName];
+                    const formattedText = formatText(columnName, columnValue);
+
+                    return (
+                      <TableCell key={pos} style={styles}>
+                        {formattedText}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default OrderHistory;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserOrderHistory } from '../../redux/Orders/orders.action';
-
+import OrderHistory from '../../components/OrderHistory/OrderHistory';
 import { Wrapper } from './Dashboard.style';
 
-const mapState = ({ user }: any) => ({
+const mapState = ({ user, ordersData }: any) => ({
   currentUser: user.currentUser,
+  orderHistory: ordersData.OrderHistory.data,
 });
 
 const Dashboard: React.FC = () => {
   const dispatch = useDispatch();
-  const { currentUser } = useSelector(mapState);
+  const { currentUser, orderHistory } = useSelector(mapState);
 
   useEffect(() => {
     dispatch(getUserOrderHistory(currentUser.id));
@@ -19,6 +20,8 @@ const Dashboard: React.FC = () => {
   return (
     <div>
       <h1>Order History</h1>
+
+      {/* <OrderHistory orders={orderHistory} /> */}
     </div>
   );
 };
